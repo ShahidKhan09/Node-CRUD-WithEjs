e189daf08a61918e6bb70b745497a350677c92b9
Muhammad Shahid <69686858+ShahidKhan09@users.noreply.github.com>
2023-04-12 10:58:08 -0700

    changes

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const winston = require('winston');
 
 module.exports = () => {
-    mongoose.connect('mongodb://localhost/customerDatabase', {
+    mongoose.connect('mongodb+srv://Shahid:123@cluster0.9tdujaw.mongodb.net/curd?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: true,
